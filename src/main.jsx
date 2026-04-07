62f00950ce4acecba3051492a707487191d173f9
unknown <musman2403>
2026-04-07 16:21:10 +0500

    Removed the clock timer and restored the original App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Timer from './components/Timer.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Timer />
+    <App />
   </StrictMode>,
 )
